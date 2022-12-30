8a0a5e9a0b2e3a0aedd43f45b917df05bf807485
Danil <bloodshoww@mail.ru>
2022-12-31 01:51:47 +0300

    card data collection

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
+// Объект с сохраненными ответами 
+const answers = {
+    2: null,
+    3: null,
+    4: null,
+    5: null
+}
+
 // Движение вперёд
 const btnNext = document.querySelectorAll('[data-nav="next"]');
 btnNext.forEach(function(button){
     button.addEventListener("click", function(){
         const thisCard = this.closest("[data-card]");
+
+
+
+        if(thisCard.dataset.validate == "novalidate") {
+            navigate("next", thisCard)
+        } else {
+            navigate("next", thisCard)
+        }
+
         navigate("next", thisCard);
 
     })
@@ -18,7 +35,7 @@ btnPrev.forEach(function(button){
     })
 })
 
-
+// Функиця для навигации вперед и назад 
 function navigate(derection, thisCard) {
 
     const thisCardNumber = parseInt(thisCard.dataset.card);
@@ -33,5 +50,38 @@ function navigate(derection, thisCard) {
     
     thisCard.classList.add("hidden")
     document.querySelector(`[data-card="${nextCard}"]`).classList.remove("hidden");
+}
 
+// Функция сбора заполненных данных  с карточки 
+function gatherCardData(number) {
+
+    let question;
+    const result = []
+
+    // Находим карточку по номеру и data атрибуту
+    const currentCard = document.querySelector(`[data-card="${number}"]`)
+
+    // Находим гланый вопрос карточки 
+    question = currentCard.querySelector("[data-question]").innerText;
+
+    // 1. Находим все заполненные значения из радио кнопок 
+    const radioValues = currentCard.querySelectorAll('[type="radio"]');
+
+    radioValues.forEach(function(item){
+        if (item.checked) {
+            result.push({
+                name: item.name,
+                value: item.value
+            })
+        }
+    })
+
+    console.log(result)
+
+    const data = {
+        question: question,
+        answer: result
+    }
+
+    return data
 }

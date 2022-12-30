895dfeee2fc1c60e058b95607bb6c99dfaca1cf8
Danil <bloodshoww@mail.ru>
2022-12-30 21:43:18 +0300

    navigate to move between cards

--- js/main.js
+++ js/main.js
@@ -2,14 +2,8 @@
 const btnNext = document.querySelectorAll('[data-nav="next"]');
 btnNext.forEach(function(button){
     button.addEventListener("click", function(){
-
         const thisCard = this.closest("[data-card]");
-        const thisCardNumber = parseInt(thisCard.dataset.card); 
-        const nextCard = thisCardNumber + 1;
-
-        thisCard.classList.add("hidden")
-
-        document.querySelector(`[data-card="${nextCard}"]`).classList.remove("hidden");
+        navigate("next", thisCard);
 
     })
 })
@@ -18,14 +12,26 @@ btnNext.forEach(function(button){
 const btnPrev = document.querySelectorAll('[data-nav="prev"]');
 btnPrev.forEach(function(button){
     button.addEventListener("click", function(){
-
         const thisCard = this.closest("[data-card]");
-        const thisCardNumber = parseInt(thisCard.dataset.card); 
-        const prevCard = thisCardNumber - 1;
-
-        thisCard.classList.add("hidden")
-
-        document.querySelector(`[data-card="${prevCard}"]`).classList.remove("hidden");
+        navigate("prev", thisCard);
 
     })
 })
+
+
+function navigate(derection, thisCard) {
+
+    const thisCardNumber = parseInt(thisCard.dataset.card);
+    
+    let nextCard;
+
+    if (derection == "next") {
+        nextCard = thisCardNumber + 1;
+    } else {
+        nextCard = thisCardNumber - 1;
+    }
+    
+    thisCard.classList.add("hidden")
+    document.querySelector(`[data-card="${nextCard}"]`).classList.remove("hidden");
+
+}
